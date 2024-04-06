e362c3a8094872c347371ddcda3c0981c7c56fc2
KM Ferdous <kmferdous.buet@gmail.com>
2024-04-06 09:50:51 +0600

    code

---
+++ src/leetcode/two-pointers/392.cs
@@ -0,0 +1,29 @@
+public bool IsSubsequence(string s, string t)
+{
+    int si = 0, ti = 0;
+
+    while (si < s.Length && ti < t.Length)
+    {
+        if (s[si] == t[ti])
+            si++;
+
+        ti++;
+    }
+
+    return si == s.Length;
+}
+
+/*
+392. Is Subsequence
+Easy
+Given two strings s and t, return true if s is a subsequence of t, or false otherwise.
+A subsequence of a string is a new string that is formed from the original string by deleting some (can be none) of the characters without disturbing the relative positions of the remaining characters. (i.e., "ace" is a subsequence of "abcde" while "aec" is not).
+
+Example 1:
+Input: s = "abc", t = "ahbgdc"
+Output: true
+
+Example 2:
+Input: s = "axc", t = "ahbgdc"
+Output: false
+*/
