1015d8a1b8213035c704190a603456d40af55a2c
KM Ferdous <kmferdous.buet@gmail.com>
2024-04-06 09:55:38 +0600

    code for Container With Most Water

---
+++ src/leetcode/two-pointers/11.cs
@@ -0,0 +1,46 @@
+public int MaxArea(int[] height)
+{
+    int s = 0, l = height.Length - 1;
+
+    int output = 0;
+
+    while (s < l)
+    {
+        output = Math.Max(output, Math.Min(height[s], height[l]) * (l - s));
+
+        if (height[s] < height[l])
+        {
+            s++;
+        }
+        else if (height[s] > height[l])
+        {
+            l--;
+        }
+        else
+        {
+            s++;
+            l--;
+        }
+    }
+
+    return output;
+}
+
+/*
+11. Container With Most Water
+Medium
+You are given an integer array height of length n. There are n vertical lines drawn 
+such that the two endpoints of the ith line are (i, 0) and (i, height[i]). Find two 
+lines that together with the x-axis form a container, such that the container contains 
+the most water. Return the maximum amount of water a container can store.
+
+Example 1:
+Input: height = [1,8,6,2,5,4,8,3,7]
+Output: 49
+Explanation: The above vertical lines are represented by array [1,8,6,2,5,4,8,3,7]. 
+In this case, the max area of water (blue section) the container can contain is 49.
+
+Example 2:
+Input: height = [1,1]
+Output: 1
+*/
